98bcd663ae71611543daef059a13730a7fb9a737
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2025-06-30 20:39:28 +0700

    Add: create backend logic for register and login

--- template-BE/src/routes/index.js
+++ template-BE/src/routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const docsRoute = require('./docs.route');
 const config = require('../config/config');
 
-const PORT = 5432;
-
 const dashboardRoutes = require('./smartandon/dashboard');
 const qccMTypesRoutes = require('./smartandon/qccMTypes');
 const lineRoutes = require('./smartandon/line');
@@ -14,10 +12,12 @@ const problemRoutes = require('./smartandon/problem');
 const oeeRoutes = require('./smartandon/OEE');
 const mtbfmttrRoutes = require('./smartandon/mtbfmttr');
 const realtimeParetoRoutes = require('./smartandon/realtimePareto');
-
+const authRoutes = require('./smartandon/auth');
+const userRoutes = require('./smartandon/user');
 
 const GaugeRoute = require('./qdc/Guage.route');
 const CommonRoute = require('./qdc/Common.route');
+const { path } = require('../app');
 
 const router = express.Router();
 
@@ -66,6 +66,14 @@ const defaultRoutes = [
     path: '/smartandon',
     route: memberRoutes,
   },
+  {
+    path: '/auth',
+    route: authRoutes,
+  },
+  {
+    path: '/user',
+    route: userRoutes,
+  }
 ];
 
 const devRoutes = [
